5e948a08384e1337ba6cca22643411f022213c36
tengxq <tengxingqiang@ouchteam.com>
2022-10-12 10:24:35 +0800

    init: project

---
+++ src/main/java/com/geektime/tdd/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.geektime.tdd;
+
+/**
+ * hello world
+ *
+ * @author tengxq
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello tdd");
+    }
+}
