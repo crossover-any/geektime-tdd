c5316eccee5b1baafa52e7cd2077dad3b31ced4a
tengxq <tengxingqiang@ouchteam.com>
2022-11-29 18:07:41 +0800

    feat: multi option

---
+++ src/main/java/com/geektime/tdd/Args.java
@@ -0,0 +1,44 @@
+package com.geektime.tdd;
+
+import com.geektime.tdd.annotation.Option;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Args
+ *
+ * @author tengxq
+ */
+public class Args {
+
+    private Args() {
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T parse(Class<T> optionClass, String... args) {
+        try {
+            List<String> argsList = Arrays.asList(args);
+            Constructor<?> constructor = optionClass.getDeclaredConstructors()[0];
+            Parameter parameter = constructor.getParameters()[0];
+            Object value = null;
+            Option option = parameter.getDeclaredAnnotation(Option.class);
+            if (parameter.getType() == Boolean.class) {
+                value = argsList.contains(option.value());
+            } else if (parameter.getType() == Integer.class) {
+               value = Integer.parseInt(argsList.get(argsList.indexOf(option.value()) + 1));
+                return (T) constructor.newInstance(value);
+            } else if (parameter.getType() == String.class) {
+                value = argsList.get(argsList.indexOf(option.value()) + 1);
+            }
+            return (T) constructor.newInstance(value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
--- src/test/java/com/geektime/tdd/ArgsTest.java
+++ src/test/java/com/geektime/tdd/ArgsTest.java
@@ -1,5 +1,12 @@
 package com.geektime.tdd;
 
+import com.geektime.tdd.annotation.Option;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * ArgsTest
  *
@@ -22,4 +29,49 @@ public class ArgsTest {
     // TODO -bool false
     // TODO -int 0
     // TODO -str ""
+
+    @Test
+    void should_set_true_if_bool_option_flag_present() {
+        BoolOption option = Args.parse(BoolOption.class, "-l");
+        assertTrue(option.logging());
+    }
+
+    @Test
+    void should_set_true_if_bool_option_flag_not_present() {
+        BoolOption option = Args.parse(BoolOption.class, "-d");
+        Assertions.assertFalse(option.logging());
+    }
+
+    @Test
+    void should_parse_int_as_integer_option_value() {
+        IntegerOption option = Args.parse(IntegerOption.class, "-p", "8080");
+        assertEquals(8080, option.port());
+    }
+
+    @Test
+    void should_parse_str_as_string_option_value() {
+        StringOption option = Args.parse(StringOption.class, "-d", "/usr/logs");
+        assertEquals("/usr/logs", option.directory());
+    }
+
+    @Test
+    void should_parse_multi_as_multi_option_value() {
+        MultiOption option = Args.parse(MultiOption.class, "-l", "-p", "8080", "-d", "/usr/log");
+        assertTrue(option.logging());
+        assertEquals(8080, option.port());
+        assertEquals("/usr/logs", option.directory());
+    }
+
+    static record BoolOption(@Option("-l") Boolean logging) {
+    }
+
+    static record IntegerOption(@Option("-p") Integer port) {
+    }
+
+    static record StringOption(@Option("-d") String directory) {
+    }
+
+    static record MultiOption(@Option("-l") Boolean logging,
+                               @Option("-p") Integer port, @Option("-d") String directory) {
+    }
 }
